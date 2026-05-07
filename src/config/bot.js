745f399ffd33a34029ea0bbca4452fece473d16e
billnews <43444813+billnews@users.noreply.github.com>
2026-05-07 20:41:53 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "/help | Mitch is the GOAT",
+        name: "Mitch is the GOAT",
         // Activity type number (0 = Playing).
         type: 3, 
       },
