280e1ce6d69919b46caddd82a0a040def3ae1795
billnews <43444813+billnews@users.noreply.github.com>
2026-05-07 20:24:51 +0200

    Update bot activity message

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Watching SOI or /help | brought to you by server boosters Jane, Rae, lily, Slava and Vasa",
+        name: "/help | Mitch is the GOAT",
         // Activity type number (0 = Playing).
         type: 3, 
       },
